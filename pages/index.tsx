1f239c9e104a8e83f6c34b7dee65bf9f5d7bbf0d
Michal Miszczyszyn <michal@mmiszy.pl>
2022-10-16 19:32:16 +0200

    Fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         href="/"
         name="Limited Edition Sports Trainer"
         thumbnailUrl="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-        price="$189.99"
+        price="18999"
         variants="6 Colors"
       />
     </ProductsLayout>
